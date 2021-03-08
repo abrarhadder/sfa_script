01bc32420135414e7304f2cc6e10b8ffc9815147
abrarhadder <79809868+abrarhadder@users.noreply.github.com>
2021-03-08 09:31:06 -0600

    Update scenefile.py
    Creating a SceneFile object and sending a Maya scene path and printing the path properties.

--- scenefile.py
+++ scenefile.py
@@ -8,15 +8,29 @@ class SceneFile(object):
         self.folder_path = path.parent
         self.ext = path.suffix
         self.descriptor, self.task, self.ver = path.stem.split("_")
+        self.ver = int(self.ver.split("v")[-1])
 
-        @property
-        def filename(self):
-            pattern = "{descriptor}_{task}_v{ver}{ext}"
-            return pattern.format(descriptor=self.descriptor,
-                                  task=self.task,
-                                  ver=self.ver,
-                                  ext=self.ext)
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
 
-        @property
-        def path(self):
-            return self.folder_path / self.filename
+    @property
+    def path(self):
+        return self.folder_path / self.filename
+
+
+scene_file = SceneFile("C:/Users/abrar/Desktop/Abrar_model_v001.ma")
+print(scene_file.folder_path)
+print(scene_file.descriptor)
+print(scene_file.task)
+print(scene_file.ver)
+print(scene_file.ext)
+print(scene_file.filename)
+print(scene_file.path)
+
+
+
